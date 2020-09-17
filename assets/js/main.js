a2b5fd39ed546d9269a01f90df314cf82c5422e1
ccanyanwu <eme42c@gmail.com>
2020-09-17 09:33:33 +0100

    Small navigation done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
-//select elements from the DOM 
-//add an event listener to clear out the disposable gif
+/*/add an event listener to clear out the disposable gif
 document.querySelector('#gif1 button').addEventListener('click', (e) => {
   e.target.parentElement.style.display = 'none';
+});*/
+
+//for the mobile navbar toggle 
+const nav = document.querySelector('nav');
+document.querySelector('#toggler').addEventListener('click', () =>{
+  nav.style.display = 'block';
+});
+
+document.querySelector('.nav-close button').addEventListener('click', () =>{
+  nav.style.display = 'none';
 });
