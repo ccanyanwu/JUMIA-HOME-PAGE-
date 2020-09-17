fb226eaead76d79eca07fb7765ae36548b1591f3
ccanyanwu <eme42c@gmail.com>
2020-09-17 04:05:43 +0100

    Made changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-//our js as we know it 
+//select elements from the DOM 
+//add an event listener to clear out the disposable gif
+document.querySelector('#gif1 button').addEventListener('click', (e) => {
+  e.target.parentElement.style.display = 'none';
+});
