b6da5f3555b0e21bd2b7a6438fb2770773407c8f
ccanyanwu <eme42c@gmail.com>
2020-09-18 20:05:37 +0100

    Added first carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,19 @@ document.querySelector('#toggler').addEventListener('click', () =>{
 document.querySelector('.nav-close button').addEventListener('click', () =>{
   nav.style.display = 'none';
 });
+
+//first slider 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.querySelectorAll('.slide');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
